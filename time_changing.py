5de380409a57d640fbd84666748de920888aa10e
Maxim Zotov <44985796+g0stly@users.noreply.github.com>
2019-09-18 16:05:38 +0300

    Create time_changing.py
    я понял, что нам все равно нужно это включить.
    посмотри, Саня, мб у тебя что попроще получится сделать по этому поводу.

---
+++ time_changing.py
@@ -0,0 +1,23 @@
+def tick(current_time='00:00'):
+
+    hours = current_time[:2]
+    minutes = current_time[3:]
+
+    if minutes == '59':
+        if hours == '23':
+            hours = '00'
+        else:
+            hours = str(int(hours) + 1)
+            if len(hours) == 1:
+                hours = '0' + hours
+        minutes = '00'
+    else:
+        minutes = str(int(minutes) + 1)
+        if len(minutes) == 1:
+            minutes = '0' + minutes
+
+    return hours + ':' + minutes
+
+
+if __name__ == '__main__':
+    print(tick('02:59'))
