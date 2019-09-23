5c9f839b37997f881b8366c3f5b3b77944bd3893
Maxim Zotov <44985796+g0stly@users.noreply.github.com>
2019-09-24 01:41:58 +0300

    minimal changes

---
+++ main versions/main (24.09).py
@@ -0,0 +1,78 @@
+import tkinter
+from winsound import *
+from datetime import *
+
+
+class Clock:
+
+    def __init__(self):
+
+        self.mode = 'clock'
+        self.alarm_time = None  # установленное время будильника
+        self.stop_time = None  # время, при котором был включен режим часов
+        self.init_time = datetime.strftime(datetime.now(), '%H:%M')  # время при первом запуске часов
+        self.flag = 0  # костыль: чтобы лишняя минута не накидывалась сходу после запуска (срабатывает after_idle)
+
+        self.main_window = tkinter.Tk()
+        self.main_window.title('digital clock')
+        self.main_window.resizable(False, False)
+
+        self.clk_label = tkinter.Label(self.main_window, width=6, font=('Ubuntu', 95),
+                                       bg='light cyan', text=self.init_time)
+        self.clk_label.grid(row=0, columnspan=3)
+
+        self.h_btn = tkinter.Button(self.main_window, bg='Misty Rose', text='H', command=lambda: self.lbl_config(60))
+        self.m_btn = tkinter.Button(self.main_window, bg='Misty Rose', text='M', command=lambda: self.lbl_config(1))
+        self.a_btn = tkinter.Button(self.main_window, bg='Misty Rose', text='A', command=self.alarm)
+
+        self.h_btn.grid(row=1, column=0, sticky='ew')
+        self.m_btn.grid(row=1, column=1, sticky='ew')
+        self.a_btn.grid(row=1, column=2, sticky='ew')
+
+        self.clk_label.after_idle(self.tick)
+        tkinter.mainloop()
+
+    def tick(self):
+
+        if self.flag == 1:
+            self.init_time = str(datetime.strptime(self.init_time, '%H:%M') + timedelta(minutes=1))[11:16]
+            self.clk_label.config(text=self.init_time)
+
+            #if self.init_time == self.alarm_time:  # все сразу крашится, но музыка идет
+            #    wake_up()
+        else:
+            self.flag = 1
+
+        self.clk_label.after(60 * 10 ** 3, self.tick)
+
+    def lbl_config(self, delta):
+
+        if delta == 60:
+            self.init_time = str(datetime.strptime(self.init_time, '%H:%M') + timedelta(minutes=delta))[11:16]
+        else:
+            self.init_time = self.init_time[:2] + \
+                             str(datetime.strptime(self.init_time[3:], '%M') + timedelta(minutes=delta))[13:16]
+
+        self.clk_label.config(text=self.init_time)
+
+    def alarm(self):
+
+        if self.mode == 'clock':
+            self.stop_time = self.init_time
+            self.mode = 'set alarm'
+        else:
+            self.alarm_time = self.init_time
+            self.init_time = self.stop_time
+            self.clk_label.config(text=self.init_time)
+            self.mode = 'clock'
+
+
+def wake_up():
+
+    filename = r'C:\Users\busya\PycharmProjects\питоню\1 - по универу\лаба 1 (гэу - 2019)\music.wav'
+    PlaySound(filename, SND_FILENAME)
+
+
+if __name__ == '__main__':
+    test = Clock()
+
