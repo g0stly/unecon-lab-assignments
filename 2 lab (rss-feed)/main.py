0e9650625976bb42c581ce3e690ce8cf7818abd1
Maxim Zotov <44985796+g0stly@users.noreply.github.com>
2019-11-14 12:51:08 +0300

    Update main.py

--- 2 lab (rss-feed)/main.py
+++ 2 lab (rss-feed)/main.py
@@ -116,7 +116,7 @@ def send_message(message):
         if ans == 1:
             bot.send_message(message.chat.id, 'новая лента добавлена.')
         else:
-            bot.send_message(message.chat.id, 'какие-то временные шоколадки с добавлением твоей ленты')
+            bot.send_message(message.chat.id, 'какие-то временные технические шоколадки с добавлением твоей ленты')
     else:
 
         if wait_for_category:
